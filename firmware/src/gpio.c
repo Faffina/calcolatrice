59fbcaa58eb697ab1ce070b71a3796cfd5a38fab
Razvan Pop <esdpop@gmail.com>
2025-12-29 05:25:52 +0100

    change name in gpio header to be consisted

--- firmware/src/gpio.c
+++ firmware/src/gpio.c
@@ -1,13 +1,2 @@
 #include "gpio.h"
 
-void gpio_init() {}
-void gpio_init_pin(
-        gpio_pin pin, 
-        gpio_mode mode, 
-        gpio_speed speed, 
-        gpio_pull_up_down_state pull_state, 
-        gpio_alternate_functions function
-) {}
-
-void write(gpio_pin pin, bool high) {}
-bool read(gpio_pin pin) {}
